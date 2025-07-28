7f7ee86d1108514096f469fdb40d34471150b0a6
Kevin Alan Martinez Virgen <kevinvr@hotmail.es>
2025-07-28 14:52:21 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 git clone https://github.com/Fairbrook/lazyvim ~/.config/nvim
-wget -O ~/.tmux.conf https://gist.githubusercontent.com/Fairbrook/79a77e4f52e155b6c87c724c13b8a72f/raw/c67c8eda15d6cfda11d9fcaa643fab2dd045a789/.tmux.conf
-
+wget https://gist.githubusercontent.com/Fairbrook/79a77e4f52e155b6c87c724c13b8a72f/raw/8e6ef361a504a80ae40c10c43da9886632c0849f/.tmux.conf -O ~/.tmux.conf 
